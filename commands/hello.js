5224c9093db888fcd645f57f1d0aecdb9d56b5ca
Glitch (boopie) <none>
2018-01-22 17:38:02 +0000

    Added some commands.

--- commands/hello.js
+++ commands/hello.js
@@ -1,3 +1,3 @@
 exports.run = (client, message, args) => {
-    message.channel.send('Hello! I am "boopie!". I hope I help you well. c: aaaaaa').catch(console.error);
+    message.channel.send('Hello! I am "boopie!". I hope I help you well. c:').catch(console.error);
 }
--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,5 @@
 exports.run = (client) => {
+  client.user.setPresence({ game: { name: 'pong!', type: 0 } });
   console.log(`boopie! is ready!`)
   console.log(`Stats: boopie! is on ${client.channels.size} channels on ${client.guilds.size} servers, for a total of ${client.users.size} users!`);
 }
